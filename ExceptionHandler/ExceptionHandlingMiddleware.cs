ed595a8d5bf3cbb31a47c7285aa49714f9d8a6bb
sam7022244383 <samimujawar7@gmail.com>
2024-10-14 14:52:39 +0530

    Added Exception Handling Middleware

---
+++ ExceptionHandler/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandler
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next)
+        { 
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpcontext)
+        {
+            try
+            {
+                await _next(httpcontext);
+            }
+            catch (Exception e)
+            {
+                await HandleExceptionAsync(httpcontext, e);
+
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            var response = context.Response;
+
+            switch (exception)
+            {
+                case ApplicationException ex:
+                    response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    break;
+                case UnauthorizedAccessException ex:
+                    response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                    break;
+                default : 
+                    response.StatusCode = (int)HttpStatusCode.InternalServerError; break;
+
+            }
+
+            await response.WriteAsync(exception.Message);
+        }
+
+    }
+}
